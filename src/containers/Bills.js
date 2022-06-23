9cf8f1af7ede5e1270890c6cd36387cef3ba3ace
Xavier Hernandez <xhernan.martinez@gmail.com>
2022-06-23 19:29:35 +0200

    Fix - Bills Modal Styling

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -23,7 +23,7 @@ export default class {
 
   handleClickIconEye = (icon) => {
     const billUrl = icon.getAttribute("data-bill-url");
-    const imgWidth = Math.floor($("#modaleFile").width() * 0.5);
+    // const imgWidth = Math.floor($("#modaleFile").width() * 0.5);
     $("#modaleFile")
       .find(".modal-body")
       .html(
