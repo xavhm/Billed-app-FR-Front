821ecb37e5ad9a93c5ccf26aac2e9b1e4b3debbb
Xavier Hernandez <xhernan.martinez@gmail.com>
2022-08-04 23:36:58 +0200

    Refacto - newBill POST test

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -10,6 +10,9 @@ import { localStorageMock } from "../__mocks__/localStorage.js";
 import BillsUI from "../views/BillsUI.js";
 import store from "../__mocks__/store.js";
 import userEvent from "@testing-library/user-event";
+import mockStore from "../__mocks__/store";
+jest.mock("../app/store", () => mockStore);
+import router from "../app/Router.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
@@ -120,106 +123,146 @@ describe("Given I am connected as an employee", () => {
 // INTEGRATION TESTS
 describe("Given I am a user connected as Employee", () => {
   describe("When I add a new bill", () => {
-    test("Then it creates a new bill", () => {
-      document.body.innerHTML = NewBillUI();
-      const inputData = {
-        type: "Transports",
-        name: "Test",
-        datepicker: "2022-06-02",
-        amount: "364",
-        vat: "80",
-        pct: "20",
-        commentary: "Test",
-        file: new File(["test"], "test.png", { type: "image/png" }),
+    beforeEach(() => {
+      jest.spyOn(store, "bills");
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+          email: "employee@test.tld",
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.appendChild(root);
+      router();
+    });
+
+    afterEach(() => {
+      jest.restoreAllMocks();
+    });
+
+    test("create a new bills from mock API POST", async () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({
+          pathname,
+        });
       };
-      const formNewBill = screen.getByTestId("form-new-bill");
-      const inputExpenseName = screen.getByTestId("expense-name");
+      window.onNavigate(ROUTES_PATH.NewBill);
+
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        store: mockStore,
+        localStorage: window.localStorage,
+      });
+
+      document.body.innerHTML = NewBillUI();
       const inputExpenseType = screen.getByTestId("expense-type");
-      const inputDatepicker = screen.getByTestId("datepicker");
+      const inputExpenseName = screen.getByTestId("expense-name");
+      const inputDatePicker = screen.getByTestId("datepicker");
       const inputAmount = screen.getByTestId("amount");
       const inputVAT = screen.getByTestId("vat");
       const inputPCT = screen.getByTestId("pct");
       const inputCommentary = screen.getByTestId("commentary");
       const inputFile = screen.getByTestId("file");
 
-      fireEvent.change(inputExpenseType, {
-        target: { value: inputData.type },
-      });
-      expect(inputExpenseType.value).toBe(inputData.type);
+      userEvent.type(inputExpenseType, "Transports");
+      userEvent.type(inputExpenseName, "Vol Paris Londres");
+      userEvent.type(inputDatePicker, "2022-04-04");
+      userEvent.type(inputAmount, "348");
+      userEvent.type(inputVAT, "70");
+      userEvent.type(inputPCT, "20");
+      userEvent.type(inputCommentary, "...");
+      newBill.fileName = "testFile";
+      newBill.fileUrl =
+        "https://test.storage.tld/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a";
+      const email = "e@e";
 
-      fireEvent.change(inputExpenseName, {
-        target: { value: inputData.name },
-      });
-      expect(inputExpenseName.value).toBe(inputData.name);
-
-      fireEvent.change(inputDatepicker, {
-        target: { value: inputData.datepicker },
-      });
-      expect(inputDatepicker.value).toBe(inputData.datepicker);
-
-      fireEvent.change(inputAmount, {
-        target: { value: inputData.amount },
-      });
-      expect(inputAmount.value).toBe(inputData.amount);
-
-      fireEvent.change(inputVAT, {
-        target: { value: inputData.vat },
-      });
-      expect(inputVAT.value).toBe(inputData.vat);
-
-      fireEvent.change(inputPCT, {
-        target: { value: inputData.pct },
-      });
-      expect(inputPCT.value).toBe(inputData.pct);
-
-      fireEvent.change(inputCommentary, {
-        target: { value: inputData.commentary },
-      });
-      expect(inputCommentary.value).toBe(inputData.commentary);
-
-      userEvent.upload(inputFile, inputData.file);
-      expect(inputFile.files[0]).toStrictEqual(inputData.file);
-      expect(inputFile.files).toHaveLength(1);
-
-      Object.defineProperty(window, "localStorage", {
-        value: {
-          getItem: jest.fn(() =>
-            JSON.stringify({
-              email: "email@test.com",
-            })
-          ),
+      const bill = [
+        {
+          id: "47qAXb6fIm2zOKkLzMro",
+          vat: "80",
+          fileUrl:
+            "https://test.storage.tld/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a",
+          status: "pending",
+          type: "Hôtel et logement",
+          commentary: "séminaire billed",
+          name: "encore",
+          fileName: "preview-facture-free-201801-pdf-1.jpg",
+          date: "2004-04-04",
+          amount: 400,
+          commentAdmin: "ok",
+          email: "a@a",
+          pct: 20,
         },
-        writable: true,
+      ];
+
+      const handleChangeFile = jest.fn(newBill.handleChangeFile);
+      const handleSubmit = jest.spyOn(newBill, "handleSubmit");
+      const formNewBill = screen.getByTestId("form-new-bill");
+      mockStore.bills.mockImplementation(() => {
+        return {
+          create: () => {
+            return Promise.resolve({ fileUrl: `${newBill.fileUrl}`, key: "1234" });
+          },
+          update: () => {
+            return Promise.resolve({
+              id: "47qAXb6fIm2zOKkLzMro",
+              vat: "80",
+              fileUrl:
+                "https://test.storage.tld/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a",
+              status: "pending",
+              type: "Hôtel et logement",
+              commentary: "séminaire billed",
+              name: "encore",
+              fileName: "preview-facture-free-201801-pdf-1.jpg",
+              date: "2004-04-04",
+              amount: 400,
+              commentAdmin: "ok",
+              email: "a@a",
+              pct: 20,
+            });
+          },
+        };
       });
 
-      const onNavigate = (pathname) => {
-        document.body.innerHTML = ROUTES({ pathname });
-      };
-
-      const newBill = new NewBill({
-        document,
-        onNavigate,
-        localStorage: window.localStorage,
-      });
-
-      const handleSubmit = jest.fn(newBill.handleSubmit);
+      inputFile.addEventListener("change", handleChangeFile);
       formNewBill.addEventListener("submit", handleSubmit);
+      userEvent.upload(
+        inputFile,
+        new File(["(--[IMG]--)"], "testFile.jpg", {
+          type: "image/jpg",
+        })
+      );
       fireEvent.submit(formNewBill);
+
+      expect(handleChangeFile).toHaveBeenCalled();
+      expect(handleChangeFile).toBeCalledTimes(1);
       expect(handleSubmit).toHaveBeenCalled();
+      expect(handleSubmit).toBeCalledTimes(1);
+      expect(mockStore.bills).toHaveBeenCalled();
+      expect(mockStore.bills).toHaveBeenCalledTimes(2);
+      expect(screen.getAllByText("Mes notes de frais")).toBeTruthy();
     });
 
-    test("Then it fails with a 404 message error", async () => {
-      const html = BillsUI({ error: "Erreur 404" });
-      document.body.innerHTML = html;
-      const message = await screen.getByText(/Erreur 404/);
-      expect(message).toBeTruthy();
-    });
+    describe("When API returns an error", () => {
+      test("Then it fails with a 404 message error", async () => {
+        const html = BillsUI({ error: "Erreur 404" });
+        document.body.innerHTML = html;
+        const message = await screen.getByText(/Erreur 404/);
+        expect(message).toBeTruthy();
+      });
 
-    test("Then it fails with a 500 message error", async () => {
-      const html = BillsUI({ error: "Erreur 500" });
-      document.body.innerHTML = html;
-      const message = await screen.getByText(/Erreur 500/);
-      expect(message).toBeTruthy();
+      test("Then it fails with a 500 message error", async () => {
+        const html = BillsUI({ error: "Erreur 500" });
+        document.body.innerHTML = html;
+        const message = await screen.getByText(/Erreur 500/);
+        expect(message).toBeTruthy();
+      });
     });
   });
 });
